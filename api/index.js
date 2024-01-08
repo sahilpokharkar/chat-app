aa5d36cb3560572e6c2f741af82bce36ed5cb6cb
CyberSam-11 <sahilpokharkar3@gmail.com>
2024-01-09 00:39:59 +0530

    changes index

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL || 'https://chat-app-nine-omega.vercel.app/',
+    origin: process.env.CLIENT_URL || 'https://chat-app-nine-omega.vercel.app',
 }));
 
 async function getUserDataFromRequest(req) {
