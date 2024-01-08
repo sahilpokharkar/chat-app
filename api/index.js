3ed159777524ff6ee1edd34512971dbec2941319
Sahil Pokharkar <97360155+sahilpokharkar@users.noreply.github.com>
2024-01-09 00:22:55 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL || 'https://chat-app-nine-omega.vercel.app',
+    origin: process.env.CLIENT_URL,
 }));
 
 async function getUserDataFromRequest(req) {
